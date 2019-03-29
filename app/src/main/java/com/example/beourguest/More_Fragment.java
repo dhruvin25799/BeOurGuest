72713a10056f73120ead9fce3696c815b139f0e2
Dhruvin Mehta <dhruvin.mehta25799@gmail.com>
2019-03-29 09:47:13 +0530

    Final

--- app/src/main/java/com/example/beourguest/More_Fragment.java
+++ app/src/main/java/com/example/beourguest/More_Fragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -35,6 +37,8 @@ public class More_Fragment extends Fragment implements View.OnClickListener {
         TextView t2 = (TextView)view.findViewById(R.id.email);
         Button logout = (Button)view.findViewById(R.id.logout);
         logout.setOnClickListener(this);
+        Button myac=(Button)view.findViewById(R.id.button3);
+        myac.setOnClickListener(this);
         t1.setText(mAuth.getCurrentUser().getDisplayName());
         t2.setText(mAuth.getCurrentUser().getEmail());
         String imgurl = mAuth.getCurrentUser().getPhotoUrl().toString();
@@ -57,6 +61,9 @@ public class More_Fragment extends Fragment implements View.OnClickListener {
                 mAuth.signOut();
                 startActivity(new Intent(getContext(),Sign_In_Screen.class));
                 Objects.requireNonNull(getActivity()).finish();
+                break;
+            case R.id.button3:
+                Toast.makeText(getContext(),"Will be added soon",Toast.LENGTH_SHORT).show();
                 break;
         }
     }
